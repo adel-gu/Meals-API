cd81351a466282110e878a329a557e2f4f302b43
adelguit <adeelguitoun@gmail.com>
2022-10-19 22:14:00 +0100

    Fix eslint errors

--- src/modules/showLike.js
+++ src/modules/showLike.js
@@ -3,11 +3,9 @@ import { getLikes } from './fetchLikes';
 const showlike = async (txtLike) => {
   const mealId = txtLike.getAttribute('data-id');
   const likesList = await getLikes();
-  const numberLikes = likesList.filter((likeObj) => {
-    return likeObj['item_id'] === mealId;
-  });
+  const numberLikes = likesList.filter((likeObj) => likeObj.item_id === mealId);
 
-  txtLike.innerHTML = `${numberLikes[0]['likes']} Likes`;
+  txtLike.innerHTML = `${numberLikes[0].likes} Likes`;
 };
 
 export default showlike;
