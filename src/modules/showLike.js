1cab322e33394023e995ef49428c46ff30bdcdbe
adelguit <adeelguitoun@gmail.com>
2022-10-19 21:54:55 +0100

    Implement the functionality for showing number of likes

---
+++ src/modules/showLike.js
@@ -0,0 +1,13 @@
+import { getLikes } from './fetchLikes';
+
+const showlike = async (txtLike) => {
+  const mealId = txtLike.getAttribute('data-id');
+  const likesList = await getLikes();
+  const numberLikes = likesList.filter((likeObj) => {
+    return likeObj['item_id'] === mealId;
+  });
+
+  txtLike.innerHTML = `${numberLikes[0]['likes']} Likes`;
+};
+
+export default showlike;
