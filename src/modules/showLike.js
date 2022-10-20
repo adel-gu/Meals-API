235fa6c3ee81c3dbb62625dae3b2e0c8a5a2e40a
adelguit <adeelguitoun@gmail.com>
2022-10-20 10:37:35 +0100

    Add condition for checking the meal item number of likes

--- src/modules/showLike.js
+++ src/modules/showLike.js
@@ -4,8 +4,10 @@ const showlike = async (txtLike) => {
   const mealId = txtLike.getAttribute('data-id');
   const likesList = await getLikes();
   const numberLikes = likesList.filter((likeObj) => likeObj.item_id === mealId);
-
-  txtLike.innerHTML = `${numberLikes[0].likes} Likes`;
+  // Check if the numberLikes array is not empty -> means we do have number of likes for the passed meal txtLike
+  if (numberLikes.length > 0) {
+    txtLike.innerHTML = `${numberLikes[0].likes} Likes`;
+  }
 };
 
 export default showlike;
