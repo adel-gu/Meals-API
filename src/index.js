095e8048c6608a1c3a71bd3138da50a709499dc2
adelguit <adeelguitoun@gmail.com>
2022-10-20 10:29:27 +0100

    Remove the related functions for meals control navs

--- src/index.js
+++ src/index.js
@@ -5,27 +5,15 @@ import showlike from './modules/showLike';
 
 // Constants
 const mealsListContainer = document.querySelector('.f-list');
-const mealsNav = document.querySelector('.control');
 
 // Populate the meals cards items
 document.addEventListener('DOMContentLoaded', async () => {
-  await showMeals(mealsListContainer, 0);
+  await showMeals(mealsListContainer);
   // Select all likes element
   const textLikes = document.getElementsByClassName('card-likes-txt');
   [...textLikes].forEach(async (textLike) => {
     await showlike(textLike);
   });
-});
-
-// Populate according to the meals-nav
-mealsNav.addEventListener('click', async (e) => {
-  e.preventDefault();
-  // Get the clicked nav link index data.
-  const navIndex = e.target.getAttribute('data-index');
-  // Empty the list meals container from the previous cards
-  mealsListContainer.innerHTML = '';
-  // Generate new 9 items
-  await showMeals(mealsListContainer, navIndex);
 });
 
 // Post Likes
